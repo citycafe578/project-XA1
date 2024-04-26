acb63c0f201978de057b59bcbba2403f82ca34d6
citycafe578 <131968937+citycafe578@users.noreply.github.com>
2024-04-26 14:50:40 +0800

    clock
    新增了一個時鐘
    雖然看得懂但是是從網路上抓來的
    但當然最後部會長那樣
    我會自己寫一個
    整理了一下CSS
    不算好看不過至少看得懂

--- web/static/script.js
+++ web/static/script.js
@@ -11,3 +11,21 @@ webcam.addEventListener("change", function() {
         image.src = imageUrl;
     }
 });
+
+function startTime(){
+    var today = new Date();
+    var hh = today.getHours();
+    var mm = today.getMinutes();
+    var ss = today.getSeconds();
+    mm = checkTime(mm);
+    ss = checkTime(ss);
+    document.getElementById('clock').innerText = hh + ":" + mm + ":" + ss;
+    var timeoutId = setTimeout(startTime, 500);
+}
+
+function checkTime(i){
+    if(i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
