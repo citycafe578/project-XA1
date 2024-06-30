ab96a4cd742119fd5872e27224b6c5d1ea909031
citycafe578 <chentim102@gmail.com>
2024-06-30 23:30:58 +0800

    雙向通訊準備

--- drone_code/drone.ino
+++ drone_code/drone.ino
@@ -2,7 +2,6 @@
 #include <RF24.h>
 #include <ESP32Servo.h>
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <TinyGPS.h>
@@ -16,18 +15,24 @@ Adafruit_BMP280 bmp;
 Servo alien;
 int alien_pin = 14;
 TinyGPS gps;
-HardwareSerial gpsSerial(2);  // Use UART2
+HardwareSerial gpsSer ial(2);  // Use UART2
 
 int data[4] = {};
 RF24 radio(17, 5); // CE, CSN 引脚
 const byte address[6] = "00001";
 
+String send;
+int16_t x = 0;
+int16_t y = 0;
+int16_t z = 0;
 float latitude = 0.0;
 float longitude = 0.0;
+int altitude = 0;
 
 void nrf_receive_Task(void *pvParam){
-  vTaskDelay(1000/portTICK_PERIOD_MS);
-  while(1){
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  while (1) {
+    radio.startListening();
     if (radio.available()) {
       char receivedData[32] = {0};
       radio.read(&receivedData, sizeof(receivedData));
@@ -41,43 +46,41 @@ void nrf_receive_Task(void *pvParam){
         token = strtok(NULL, " ");
       }
 
-      // for (int i = 0; i < 4; i++) {
-      //   Serial.print(data[i]);
-      //   Serial.print(" ");
-      // }
-      // Serial.println();
+      for (int i = 0; i < 4; i++) {
+        Serial.print(data[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
     }
     alien.write(data[3]);
     vTaskDelay(50 / portTICK_PERIOD_MS);
+    radio.stopListening();
+    // send = String(x) + " " + String(y) + " " + String(z) + " " + String(latitude, 6) + " " + String(longitude, 6) + " " + String(altitude);
   }
 }
 
 void i2c_Task(void *pvParam){
-  vTaskDelay(1000/portTICK_PERIOD_MS);
-  while(1){
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  while (1) {
     readAccelerometer();
     readBMP();
   }
-
 }
 
 void gps_Task(void *pvParam){
-  vTaskDelay(1000/portTICK_PERIOD_MS);
-  while(1){
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  while (1) {
     bool newData = false;
 
-    for (unsigned long start = millis(); millis() - start < 1000;)
-    {
-      while (gpsSerial.available())
-      {
+    for (unsigned long start = millis(); millis() - start < 1000;) {
+      while (gpsSerial.available()) {
         char c = gpsSerial.read();
         if (gps.encode(c))
           newData = true;
       }
     }
 
-    if (newData)
-    {
+    if (newData) {
       float flat, flon;
       unsigned long age;
       gps.f_get_position(&flat, &flon, &age);
@@ -85,49 +88,34 @@ void gps_Task(void *pvParam){
       latitude = (flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat);
       longitude = (flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon);
 
-      Serial.print("LAT=");
-      Serial.print(latitude, 6);
-      Serial.print(" LON=");
-      Serial.println(longitude, 6);
+      // Serial.print("LAT=");
+      // Serial.print(latitude, 6);
+      // Serial.print(" LON=");
+      // Serial.println(longitude, 6);
       vTaskDelay(500 / portTICK_PERIOD_MS);
-    }else{
-      Serial.println("no gps data");
+    } else {
+      // Serial.println("no gps data");
     }
   }
 }
-
-// void nrf_send_Task(void *pvParam){
-//   vTaskDelay(1000/portTICK_PERIOD_MS);
-//   while(1){
-
-//   }
-// }
 
 void setup(){
   Serial.begin(9600);
   xTaskCreatePinnedToCore(nrf_receive_Task, "nrf_receive_Task", 1024, NULL, 2, NULL, 0);
   xTaskCreatePinnedToCore(i2c_Task, "i2c_Task", 2048, NULL, 1, NULL, 1);
   xTaskCreatePinnedToCore(gps_Task, "gps_Task", 1024, NULL, 1, NULL, 1);
-  // xTaskCreatePinnedToCore(nrf_send_Task, "nrf_send_Task", 1024, NULL, 1, NULL, 0);
-  vTaskDelay(2000/portTICK_PERIOD_MS);
+  vTaskDelay(2000 / portTICK_PERIOD_MS);
   radio.begin();
   radio.openReadingPipe(1, address);
   radio.setPALevel(RF24_PA_LOW);
-  radio.startListening();
   alien.attach(14);
   Wire.begin(SDA, SCL);
   initAccelerometer();
-  if (!bmp.begin(BMP280_add)) {
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    while (1);
-  }
   gpsSerial.begin(4800, SERIAL_8N1, 14, 12);
   delay(500);
 }
 
-void loop(){
-
-}
+void loop(){}
 
 void initAccelerometer(){
   Wire.beginTransmission(GY511_add);
@@ -142,7 +130,7 @@ void readAccelerometer(){
   Wire.endTransmission();
   
   Wire.requestFrom(GY511_add, 6);
-  if (Wire.available() == 6){
+  if (Wire.available() == 6) {
     int16_t x = Wire.read() | (Wire.read() << 8);
     int16_t y = Wire.read() | (Wire.read() << 8);
     int16_t z = Wire.read() | (Wire.read() << 8);
@@ -154,6 +142,7 @@ void readAccelerometer(){
 }
 
 void readBMP(){
+  altitude = bmp.readAltitude();
   // Serial.print(bmp.readAltitude(1013.25)); 
   // Serial.println(" m   ");
   // Serial.print(bmp.readTemperature()); 
